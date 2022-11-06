5a3d70f27b51257b3a0b0a85af66712212e8c6b2
Matt Gaidica <matt@gaidi.ca>
2022-11-06 12:55:40 -0500

    Saving to flash, calibrating works, ready for BLE

--- Gripr.ino
+++ Gripr.ino
@@ -1,17 +1,18 @@
 #include <ATSAMD21_ADC.h>
 #include <LinearRegression.h>
-#include "RunningAverage.h"
+#include <RunningAverage.h>
+#include <FlashStorage_SAMD.h>
 
 LinearRegression lr = LinearRegression();
-double linReg[2];
+double linReg[2] = { 0.172, -1.533 };
 double actualLoad = 0;
+uint16_t eeAddress = 0;
 
 const int nSamples = 50;
 RunningAverage myRA(nSamples);
 int sampleCount = 0;
 
 int16_t adcVal = 0;
-float b;
 int samplesCount = 0;
 
 const int neg_pin = A0;  //A6;
@@ -22,23 +23,29 @@ long int displayTime;
 
 void setup() {
   Serial.begin(9600);
-  delay(1000);
+  while (!Serial) {}
   // analogReset();
   analogGain(ADC_GAIN_16);
   analogReadExtended(15);  // 16-bit samples @ 108ms
   analogReference2(ADC_REF_INTVCC1);
   analogCalibrate();
 
-  // calibrate load sensor
-  // myRA.clear();
-  // for (sampleCount = 0; sampleCount < nSamples; sampleCount++) {
-  //   adcVal = analogDifferential(pos_pin, neg_pin);
-  //   myRA.addValue(float(adcVal));
-  // }
-  // b = myRA.getAverage();
-  // myRA.clear();
-
-  // calibrateLoad();
+  double eeprom_linReg[2] = { 0, 0 };
+  EEPROM.get(eeAddress, eeprom_linReg[0]);
+  EEPROM.get(eeAddress + sizeof(double), eeprom_linReg[1]);
+  if (!isnan(eeprom_linReg[0]) & !isnan(eeprom_linReg[1])) {
+    Serial.println("FLASH RECOVERED");
+    Serial.print(eeprom_linReg[0], HEX);
+    Serial.print(" - ");
+    Serial.println(eeprom_linReg[1], HEX);
+    linReg[0] = eeprom_linReg[0];
+    linReg[1] = eeprom_linReg[1];
+  } else {
+    Serial.println("FLASH NAN");
+  }
+  lr.reset();
+  lr.learn(0.0, linReg[0]);
+  lr.learn(200.0, (200 * linReg[0]) + linReg[1]);  // y = m*x + b
 
   displayTime = millis();
 }
@@ -52,7 +59,7 @@ void loop() {
   }
   sampleCount++;
 
-  if (millis() > displayTime + DISPLAY_MS) { // !! split these up
+  if (millis() > displayTime + DISPLAY_MS) {  // !! split these up
     displayTime = millis();
     Serial.print("300, -300, ");
     // Serial.print(String(myRA.getAverage()) + ", ");
@@ -65,6 +72,7 @@ void loop() {
 }
 
 void calibrateLoad() {
+  float avg;
   String str = Serial.readString();
 
   if (str.indexOf("cal") >= 0) {
@@ -79,10 +87,9 @@ void calibrateLoad() {
       adcVal = analogDifferential(pos_pin, neg_pin);
       myRA.addValue(float(adcVal));
     }
-    b = myRA.getAverage();
+    avg = myRA.getAverage();
     myRA.clear();
-    Serial.println(b);
-    lr.learn(b, 0);
+    lr.learn(avg, 0);
 
     Serial.println("Apply load - how many grams?");
     while (Serial.available() == 0) {}
@@ -94,15 +101,19 @@ void calibrateLoad() {
       adcVal = analogDifferential(pos_pin, neg_pin);
       myRA.addValue(float(adcVal));
     }
-    b = myRA.getAverage();
+    avg = myRA.getAverage();
     myRA.clear();
-    Serial.println(b);
-    lr.learn(b, loadGrams);
+    lr.learn(avg, loadGrams);
 
     lr.parameters(linReg);
     char buffer[30];
-    sprintf(buffer, "y = %1.1fx + %1.1f", linReg[0], linReg[1]);
+    sprintf(buffer, "y = %1.3fx + %1.3f", linReg[0], linReg[1]);
     Serial.println(buffer);
+
+    EEPROM.put(eeAddress, linReg[0]);
+    EEPROM.put(eeAddress + sizeof(double), linReg[1]);
+    EEPROM.commit();
+    Serial.println("SAVED TO FLASH");
 
     delay(2000);
   }
